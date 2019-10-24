000d986146f417ec6f04c8d6f66c169e782d15c6
Carlos Moratelli <carlos.moratelli@ufsc.br>
2019-10-24 12:34:42 -0300

    Generic compilation between ESP32 and ESP8266.

--- ThingerESP.ino
+++ ThingerESP.ino
@@ -1,22 +1,34 @@
+#ifdef ESP32
 #include <ThingerESP32.h>
+#else
+#include <ThingerESP8266.h>
+#endif
+
 #include <DHT.h>
 
-#define USERNAME "crmoratelli"
-#define DEVICE_ID "ESP32"
+#define USERNAME "username"
+#define DEVICE_ID "ESP"
 #define DEVICE_CREDENTIAL "Teste12345"
 
 #define SSID "Curso_IoT"
 #define SSID_PASSWORD "0123456789"
 
 /* Configuraçao da conexao com o Thinger.io */
+#ifdef ESP32
+#define DHTPIN 4
 ThingerESP32 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+#else
+#define DHTPIN D4
+ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+#endif
 
 /* Configuraçao do sensor de temperatura e umidade.
    Pino, Modelo
 */
-DHT dht(4, DHT11);
+DHT dht(DHTPIN, DHT11);
 
 void setup() {
+
   /* Inicia conexao com o thinger.io */   
   thing.add_wifi(SSID, SSID_PASSWORD);
 
